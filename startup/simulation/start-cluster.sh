dcae79baa6d4399f83fb896db462f9c17cf462f8
Tim Hsieh <timpeihunghsieh@gmail.com>
2021-05-16 20:35:27 -0400

    Added gcloud simulation support

---
+++ startup/simulation/start-cluster.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Istio Gateway
+./deployments/istio/startup.sh
+
+# Prometheus stack
+# ./deployments/prometheus-stack/startup.sh
+
+# # ELK
+# ./deployments/elk-stack/startup.sh
+
+bazel run deployments/greeter-server/simulation:k8-all.apply
+bazel run deployments/FrontendService/simulation:k8-all.apply
