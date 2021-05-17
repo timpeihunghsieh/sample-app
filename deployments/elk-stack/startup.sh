24535f0e6de9827ecfa106529a35ff042e5b4dbb
Tim Hsieh <timpeihunghsieh@gmail.com>
2021-05-16 21:15:18 -0400

    Commited missed files

--- deployments/elk-stack/startup.sh
+++ deployments/elk-stack/startup.sh
@@ -4,3 +4,5 @@ kubectl create namespace logs-monitoring
 kubectl apply -f https://download.elastic.co/downloads/eck/1.5.0/all-in-one.yaml
 sleep 5  # TODO(timhsieh): hack to wait for CRD to be deployed. 
 kubectl apply -f deployments/elk-stack/filebeat_autodiscover.yaml --namespace logs-monitoring
+
+kubectl apply -f deployments/elk-stack/virtual-service.yaml
